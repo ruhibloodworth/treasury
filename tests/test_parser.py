ffaedd4048ce67df9945e7de2fe637cc0e93bdbf
Ruhi Bloodworth <ruhi.bloodworth@gmail.com>
2022-09-16 05:27:17 +0000

    🚧: parse an open or tr entry with lark

---
+++ tests/test_parser.py
@@ -0,0 +1,22 @@
+from decimal import Decimal
+from lark import Tree, Token
+from treasury.parser import parser
+
+
+def test_parse_open_ok():
+    tree = parser.parse("2022-03-04 OPEN Checking 123.04")
+    assert tree == Tree('start', [
+        Tree('open_entry', [
+            Tree('date', [2022, 3, 4]),
+            Tree('account', [Token('WORD', 'Checking')]),
+            Tree('amount', [Decimal('123.04')])])])
+
+
+def test_parse_tr_ok():
+    tree = parser.parse("2022-03-04 TR Checking Savings 123.04")
+    assert tree == Tree('start', [
+        Tree('tr_entry', [
+            Tree('date', [2022, 3, 4]),
+            Tree('account', [Token('WORD', 'Checking')]),
+            Tree('account', [Token('WORD', 'Savings')]),
+            Tree('amount', [Decimal('123.04')])])])
---
+++ treasury/parser.py
@@ -0,0 +1,37 @@
+from decimal import Decimal
+from lark import Lark, Transformer
+
+
+class T(Transformer):
+    pass
+    YEAR = int
+    MONTH = int
+    DAY = int
+    SIGNED_NUMBER = Decimal
+
+
+parser = Lark(r"""
+    %ignore WS_INLINE
+
+    start: entry
+
+    ?entry: open_entry
+          | tr_entry
+
+    open_entry: date "OPEN" account amount
+    tr_entry: date "TR" account account amount
+
+    date: YEAR "-" MONTH "-" DAY
+    YEAR: /[0-9]{4}/
+    MONTH: /[0-9]{2}/
+    DAY: /[0-9]{2}/
+
+    account: WORD
+
+    amount: SIGNED_NUMBER
+
+    %import common.DIGIT
+    %import common.SIGNED_NUMBER
+    %import common.WORD
+    %import common.WS_INLINE
+""", transformer=T(), parser="lalr")
